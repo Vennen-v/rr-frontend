ff70779da257974253b455ba621075551d1058f4
Vennen-v <chgolliday@gmail.com>
2025-06-02 02:03:28 -0500

    quick fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,13 +5,4 @@ const api = axios.create({
   withCredentials: true,
 });
 
-async function currentUserInfo() {
-  try {
-    const { data } = await api.get(`/user`);
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 export default api;
