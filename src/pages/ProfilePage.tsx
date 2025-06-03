6aa42e1826b11dcf32a1e37cb0882e71800491e6
Vennen-v <chgolliday@gmail.com>
2025-06-02 19:54:24 -0500

    profile page loader and font adjustment

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -17,6 +17,7 @@ function ProfilePage() {
     user?.followers.length
   );
   const [isFollowing, setIsFollowing] = useState<boolean>();
+  const [isLoading, setIsLoading] = useState<boolean | undefined>();
   const navigate = useNavigate();
   const location = useLocation();
 
@@ -27,6 +28,7 @@ function ProfilePage() {
   }, [location.pathname, isFollowing?.valueOf]);
 
   async function fetchUserInfo() {
+    setIsLoading(true);
     try {
       const { data } = await api.get(`/user/${userName}`);
       setUser(data);
@@ -34,6 +36,8 @@ function ProfilePage() {
     } catch (error) {
       // console.log(error);
       navigate("*");
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -102,122 +106,132 @@ function ProfilePage() {
 
   return (
     <div className=" flex flex-col flex-1 w-full md:w-fit h-screen mx-auto overflow-y-auto bg-[#141414]">
-      <div className=" h-18 border-b border-b-gray-500 w-full text-center text-2xl text-[#eeeeee] font-bold p-5">
-        {user?.displayName}
-      </div>
-      <div className="h-13 mx-auto w-full flex items-center gap-2 border-b text-[#eeeeee] border-b-gray-500">
-        <div role="tablist" className="tabs tabs-border h-full m-auto">
-          <button
-            onClick={() => handleTabChange("tab 1")}
-            role="tab"
-            className={`tab ${activeTab === "tab 1" && "tab-active"}
-              `}
-          >
-            Posts
-          </button>
-          <button
-            onClick={() => handleTabChange("tab 2")}
-            role="tab"
-            className={`tab ${activeTab === "tab 2" && "tab-active"}`}
-          >
-            About
-          </button>
-        </div>
-      </div>
-
-      <div className="container w-max mx-auto rounded-md flex flex-col h-full">
-        <div className="flex flex-col gap-5 h-100 w-92 sm:w-80 md:w-145 mx-auto mt-10 text-[#eeeeee]">
-          <div className="flex flex-col gap-4 justify-between md:mx-5">
-            <div className="flex gap-3 justify-between items-center w-full">
-              <div>
-                <div className="text-lg md:text-base">@{user?.userName}</div>
-                <div className=" flex gap-2  text-[#a8a8a8]">
-                  <span className="text-sm md:text-base">
-                    {followCount || user?.followers.length} Followers
-                  </span>
-                  <span className="text-sm md:text-base">•</span>
-                  <span className="text-smmd:text-base">
-                    {user?.userPosts.length} Posts
-                  </span>
-                </div>
-              </div>
-              <img
-                className="object-cover rounded-md h-22 w-22 "
-                src={user?.profilePic}
-              />
-            </div>
-            <div className="text-sm md:text-base">{user?.bio}</div>
-
-            {currUser?.userName !== userName && !isFollowing && currUser && (
-              <button
-                onClick={(e) => FollowUser(e)}
-                className="w-1/4 p-2 bg-[#8956FB] rounded-lg duration-300 ease-in-out hover:bg-[#674b9b] hover:cursor-pointer"
-              >
-                Follow
-              </button>
-            )}
-
-            {currUser?.userName !== userName && isFollowing && currUser && (
-              <button
-                onClick={(e) => FollowUser(e)}
-                className="w-1/4 p-2  rounded-lg border border-gray-500 duration-300 ease-in-out hover:bg-[#383838] hover:cursor-pointer"
-              >
-                Following
-              </button>
-            )}
-            {currUser?.userName == userName && (
-              <Link
-                to={"/settings"}
-                className="w-1/3 md:w-1/5 p-1 ml-auto rounded-full border border-gray-500 duration-300 ease-in-out hover:bg-[#383838]  hover:cursor-pointer text-sm text-center"
-              >
-                Edit Profile
-              </Link>
-            )}
+      {isLoading ? (
+        <span className="loading loading-spinner loading-md m-auto"></span>
+      ) : (
+        <>
+          <div className=" h-18 border-b border-b-gray-500 w-full text-center text-2xl text-[#eeeeee] font-bold p-5">
+            {user?.displayName}
           </div>
-          <div className="divider"></div>
-          {activeTab === "tab 1" && (
-            <div className="flex flex-col gap-4">
-              {postPages && postPages.content.length > 0 ? (
-                postPages?.content.map((p: Posts) => (
-                  <Post
-                    key={p.postId}
-                    postId={p.postId}
-                    userName={p.userName}
-                    title={p.title}
-                    comments={p.comments}
-                    displayName={p.displayName}
-                    profilePic={p.profilePic}
-                    postImg={p.postImg}
-                    saves={p.saves}
-                    content={p.content}
-                    likes={p.likes}
-                    createdAt={p.createdAt}
+          <div className="h-13 mx-auto w-full flex items-center gap-2 border-b text-[#eeeeee] border-b-gray-500">
+            <div role="tablist" className="tabs tabs-border h-full m-auto">
+              <button
+                onClick={() => handleTabChange("tab 1")}
+                role="tab"
+                className={`tab ${activeTab === "tab 1" && "tab-active"}
+              `}
+              >
+                Posts
+              </button>
+              <button
+                onClick={() => handleTabChange("tab 2")}
+                role="tab"
+                className={`tab ${activeTab === "tab 2" && "tab-active"}`}
+              >
+                About
+              </button>
+            </div>
+          </div>
+
+          <div className="container w-max mx-auto rounded-md flex flex-col h-full">
+            <div className="flex flex-col gap-5 h-100 w-92 sm:w-80 md:w-145 mx-auto mt-10 text-[#eeeeee]">
+              <div className="flex flex-col gap-4 justify-between md:mx-5">
+                <div className="flex gap-3 justify-between items-center w-full">
+                  <div>
+                    <div className="text-lg md:text-base">
+                      @{user?.userName}
+                    </div>
+                    <div className=" flex gap-2  text-[#a8a8a8]">
+                      <span className="text-sm md:text-base">
+                        {followCount || user?.followers.length} Followers
+                      </span>
+                      <span className="text-sm md:text-base">•</span>
+                      <span className="text-sm md:text-base">
+                        {user?.userPosts.length} Posts
+                      </span>
+                    </div>
+                  </div>
+                  <img
+                    className="object-cover rounded-md h-22 w-22 "
+                    src={user?.profilePic}
                   />
-                ))
-              ) : (
-                <div className="text-center">No posts yet</div>
+                </div>
+                <div className="text-sm md:text-base">{user?.bio}</div>
+
+                {currUser?.userName !== userName &&
+                  !isFollowing &&
+                  currUser && (
+                    <button
+                      onClick={(e) => FollowUser(e)}
+                      className="w-1/4 p-2 bg-[#8956FB] rounded-lg duration-300 ease-in-out hover:bg-[#674b9b] hover:cursor-pointer"
+                    >
+                      Follow
+                    </button>
+                  )}
+
+                {currUser?.userName !== userName && isFollowing && currUser && (
+                  <button
+                    onClick={(e) => FollowUser(e)}
+                    className="w-1/4 p-2  rounded-lg border border-gray-500 duration-300 ease-in-out hover:bg-[#383838] hover:cursor-pointer"
+                  >
+                    Following
+                  </button>
+                )}
+                {currUser?.userName == userName && (
+                  <Link
+                    to={"/settings"}
+                    className="w-1/3 md:w-1/5 p-1 ml-auto rounded-full border border-gray-500 duration-300 ease-in-out hover:bg-[#383838]  hover:cursor-pointer text-sm text-center"
+                  >
+                    Edit Profile
+                  </Link>
+                )}
+              </div>
+              <div className="divider"></div>
+              {activeTab === "tab 1" && (
+                <div className="flex flex-col gap-4">
+                  {postPages && postPages.content.length > 0 ? (
+                    postPages?.content.map((p: Posts) => (
+                      <Post
+                        key={p.postId}
+                        postId={p.postId}
+                        userName={p.userName}
+                        title={p.title}
+                        comments={p.comments}
+                        displayName={p.displayName}
+                        profilePic={p.profilePic}
+                        postImg={p.postImg}
+                        saves={p.saves}
+                        content={p.content}
+                        likes={p.likes}
+                        createdAt={p.createdAt}
+                      />
+                    ))
+                  ) : (
+                    <div className="text-center">No posts yet</div>
+                  )}
+                </div>
+              )}
+              {activeTab === "tab 2" && (
+                <div className="flex flex-col gap-5 items-center w-90 md:w-140 mx-auto">
+                  <div className="bg-[#202020] p-3 rounded-md text-sm w-full">
+                    Joined: {user?.createdAt.slice(0, 10)}
+                  </div>
+                  {/* <div className="flex justify-around bg-[#202020] p-3 rounded-md">
+                <div className="flex flex-col gap-2 items-center">
+                <span>Total Likes</span>
+                <span></span>
+                </div>
+                <div className="flex flex-col gap-2 items-center">
+                <div>Total Bookmarks</div>
+                <span>5</span>
+                </div>
+                </div> */}
+                </div>
               )}
             </div>
-          )}
-          {activeTab === "tab 2" && (
-            <div className="flex flex-col gap-5 items-center w-90 md:w-140 mx-auto">
-              <div className="bg-[#202020] p-3 rounded-md text-sm w-full">
-                Joined: {user?.createdAt.slice(0, 10)}
-              </div>
-              {/* <div className="flex justify-around bg-[#202020] p-3 rounded-md">
-                <div className="flex flex-col gap-2 items-center">
-                  <span>Total Likes</span>
-                  <span></span>
-                </div>
-                <div className="flex flex-col gap-2 items-center">
-                  <div>Total Bookmarks</div>
-                  <span>5</span>
-                </div>
-              </div> */}
-            </div>
-          )}
-        </div>
-      </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
